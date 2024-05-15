2fb0a7c4466795a12972778de1bc011effabd135
AJ Sherrell <ajsherrell.ajacks@gmail.com>
2024-05-15 14:56:17 -0400

    Change test case annotations.

--- app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
@@ -3,9 +3,10 @@ package com.plcoding.testingcourse.shopping.domain
 import assertk.assertFailure
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class ShoppingCartTest {
 
@@ -16,20 +17,36 @@ internal class ShoppingCartTest {
         cart = ShoppingCart()
     }
 
-    @Test
-    fun `Add multiple products, total price sum is correct`() {
+    // Instead of using JUnit's @Test annotation, we use ParameterizedTest to run the test multiple times.
+    @ParameterizedTest
+//    @ValueSource(
+//        ints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//    )
+    @CsvSource(
+        "1, 12.0",
+        "0, 0.0",
+        "3, 3.0",
+        "4, 1.0",
+        "5, 13.0",
+        "6, 16.0",
+        "7, 5.0",
+        "8, 9.0",
+        "9, 2.0",
+        "10, 4.0"
+    )
+    fun `Add multiple products, total price sum is correct`(quantity: Int, expectedPrice: Double) {
         // GIVEN
-        val product = Product(1, "Product", 10.0)
-        cart.addProduct(product, 3)
+        val product = Product(1, "Product", expectedPrice)
+        cart.addProduct(product, quantity)
 
         // ACTION
         val totalCost = cart.getTotalCost()
 
         // ASSERT
-        assertThat(totalCost).isEqualTo(30.0)
+        assertThat(totalCost).isEqualTo(quantity * expectedPrice)
     }
 
-    @Test
+    @RepeatedTest(50) // Needed if testing for a flaky test.
     fun `Add product with negative quantity, throw exception`() {
         // GIVEN
         val product = Product(1, "Product", 10.0)
